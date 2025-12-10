95d2b892ce098510ce533994d7f074c495edea2b
Marco Dal Pino <madalpin@microsoft.com>
2025-12-10 09:12:15 +0100

    * 'Line 1: Add the correct using directive for ObservablePropertyAttribute, which is defined in CommunityToolkit.Mvvm.ComponentModel. This ensures that the ObservableProperty attribute is recognized.' in file 'TrafficTrack.App\ViewModels\MainViewModel.cs'

--- TrafficTrack.App/ViewModels/MainViewModel.cs
+++ TrafficTrack.App/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using TrafficTrack.App.Models;
 using TrafficTrack.App.Services;
